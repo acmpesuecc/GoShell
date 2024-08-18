b5bca13b3f502a661e1ba886caf75ad3121a6a70
IshaanNene <ishaannene@gmail.com>
2024-08-18 15:10:36 +0530

    Touch With {1..10}/{a..j}

--- internal/core/touch_commands.go
+++ internal/core/touch_commands.go
@@ -12,23 +12,31 @@ var TouchCmd = &cobra.Command{
 	Use:   "touch [filename]",
 	Short: "Create a new file",
 	Long:  `The touch command creates a new file with the given filename.`,
-	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fileName := args[0]
-		_, err := os.Stat(fileName)
-		if os.IsNotExist(err) {
-			err = os.WriteFile(fileName, []byte(""), 0666)
-			if err != nil {
-				log.Fatalf("Failed to create file: %v", err)
-			} else {
-				fmt.Println("File created successfully")
+		if len(args) == 1 {
+			File_Maker(args[0])
+		} else if len(args) > 1 {
+			for _, i := range args[:] {
+				File_Maker(i)
 			}
-		} else if err == nil {
-			fmt.Println("File already exists")
-		} else {
-			log.Fatalf("Failed to check if file exists: %v", err)
 		}
 	},
+}
+
+func File_Maker(fileName string) {
+	_, err := os.Stat(fileName)
+	if os.IsNotExist(err) {
+		err = os.WriteFile(fileName, []byte(""), 0666)
+		if err != nil {
+			log.Fatalf("Failed to create file: %v", err)
+		} else {
+			fmt.Println("File created successfully")
+		}
+	} else if err == nil {
+		fmt.Println("File already exists")
+	} else {
+		log.Fatalf("Failed to check if file exists: %v", err)
+	}
 }
 
 //
